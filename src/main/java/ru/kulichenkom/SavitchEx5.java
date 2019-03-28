c5c84bc1031a32dfa5738671d5e3293d5791f303
Maxim <kulichenkom@ya.ru>
2019-03-28 23:26:18 +0300

    exercise

---
+++ src/main/java/ru/kulichenkom/SavitchEx5.java
@@ -0,0 +1,24 @@
+
+package ru.kulichenkom;
+import java.util.Scanner;
+
+public class SavitchEx5 {
+    public static void main(String[] args) {
+
+        System.out.println("Enter a phrase without punctuation:");
+        Scanner in = new Scanner(System.in);
+        String words = in.nextLine();
+        in.close();
+        int gap_1, gap_2;
+        gap_1 = words.indexOf(" ");
+        gap_2 = words.lastIndexOf(" ");
+        String words1 = words.substring(gap_1);
+        String words2 = words.substring(gap_2);
+        System.out.println("Yor phrase: " + words2 + " " + words1);
+
+
+
+
+    }
+
+}
