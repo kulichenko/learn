ac2ad166b5bb4f4f23602aaf8b26ec13b58b292c
d.tuchspropellerads.net <d.tuchs@propellerads.net>
2019-03-28 14:55:46 +0300

    maven project structure

---
+++ src/main/test/ru/kulichenkom/SimpleTest.java
@@ -0,0 +1,12 @@
+package ru.kulichenkom;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SimpleTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(true);
+    }
+}
