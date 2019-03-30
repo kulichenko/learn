deffbb12484864d2d9e33221c986b1d20d7365d4
Maxim <kulichenkom@ya.ru>
2019-03-31 00:01:12 +0300

    Chapter2 Exercise 11
    Signed-off-by: Maxim <kulichenkom@ya.ru>

---
+++ src/main/java/ru/kulichenkom/SavitchCh2Ex11.java
@@ -0,0 +1,20 @@
+package ru.kulichenkom;
+
+import javax.swing.*;
+
+public class SavitchCh2Ex11 {
+    public static void main(String[] args) {
+        String appleString;
+        appleString = JOptionPane.showInputDialog("Введите количество яблок");
+        int appleCount;
+        appleCount = Integer.parseInt(appleString);
+        String clamsPerApple;
+        clamsPerApple = JOptionPane.showInputDialog("Введите стоимость 1 яблока");
+        int clamsPerAppleCount = Integer.parseInt(clamsPerApple);
+        int totalNumbersOfClams;
+        totalNumbersOfClams = appleCount * clamsPerAppleCount;
+        JOptionPane.showMessageDialog(null, "Стоимость всех яблок " + totalNumbersOfClams);
+
+        System.exit(0);
+    }
+}
