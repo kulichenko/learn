c93b7d7b6e7c73677197f4846a32557510d61c90
Maxim <kulichenkom@ya.ru>
2019-04-01 23:32:37 +0300

    getter & setter
    Signed-off-by: Maxim <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/EkkelCh2Ex1.java
+++ src/main/java/ru/kulichenkom/EkkelCh2Ex1.java
@@ -3,12 +3,28 @@
 package ru.kulichenkom;
 
 public class EkkelCh2Ex1 {
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public char getCh() {
+        return ch;
+    }
+
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
+
     private int i;
     private char ch;
 
     public static void main(String[] arg) {
         EkkelCh2Ex1 testObject = new EkkelCh2Ex1();
-        System.out.println("integer is :" + testObject.i);
-        System.out.println("Char is: " + testObject.ch);
+        System.out.println("integer is :" + testObject.getI());
+        System.out.println("Char is: " + testObject.getCh());
     }
 }
