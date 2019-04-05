8cf628f1ae3c52235dd637dea39a63b9f58ef524
Maxim <kulichenkom@ya.ru>
2019-04-05 22:49:12 +0300

    import static...
    Signed-off-by: Maxim <kulichenkom@ya.ru>

---
+++ src/main/java/ShortPrintln.java
@@ -0,0 +1,12 @@
+//Глава 3. Упр 1. Напишите програаму, в которой используются как короткие, так и длинные команды печати.
+//static означает, что метод связан с классом, а не с конкретным экземпляром (объектом) этого класса.
+// Это означает, что вы можете вызвать статический метод, не создавая объект класса.
+
+import static ru.kulichenkom.utils.Print.print;
+
+public class ShortPrintln {
+    public static void main(String[] args) {
+        System.out.println("Standard println");
+        print("Short method of print");
+    }
+}
