d47ffbee31b4894141d28c3ab3f96ba24aa9c125
Maxim <kulichenkom@ya.ru>
2019-04-07 00:08:44 +0300

    Математические операторы
    Signed-off-by: Maxim <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/chapter3/Velocity.java
+++ src/main/java/ru/kulichenkom/chapter3/Velocity.java
@@ -4,21 +4,20 @@ package ru.kulichenkom.chapter3;
 import java.util.Scanner;
 
 public class Velocity {
-    static double distance, time;
+    public static double distance, time;
 
-    static double velocityCalculateX5(Velocity value) {
+    static double velocityCalculateX5() {
         return distance = distance * 5;
     }
 
     public static void main(String[] args) {
-        Velocity goplk = new Velocity();
         System.out.println("Enter distance");
         Scanner in = new Scanner(System.in);
         distance = in.nextDouble();
         System.out.println("Enter time");
         time = in.nextDouble();
-        System.out.println("Time is " + time + " Distance is " + distance + "\n Velocity is " + distance / time);
-        velocityCalculateX5(goplk);
+        System.out.println("Time is " + time + " Distance is " + distance + "\n Velocity is " + distance / time + "\n");
+        velocityCalculateX5();
         System.out.println("Time is " + time + " Distance is " + distance + "\n Velocity is " + distance / time);
     }
 }
