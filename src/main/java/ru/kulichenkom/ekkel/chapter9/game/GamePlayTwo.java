dba97eecab6b01de8849d98b646f93c707d769ad
Maxim <kulichenkom@ya.ru>
2019-05-26 22:48:55 +0300

    Chapter 9 Ex 19 - абстрактная фабрика
    Signed-off-by: Maxim <kulichenkom@ya.ru>

---
+++ src/main/java/ru/kulichenkom/ekkel/chapter9/game/GamePlayTwo.java
@@ -0,0 +1,18 @@
+package ru.kulichenkom.ekkel.chapter9.game;
+
+import ru.kulichenkom.ekkel.chapter9.game.factories.FactoryOfGames;
+import ru.kulichenkom.ekkel.chapter9.game.interfaces.Game;
+
+import java.util.Scanner;
+
+public class GamePlayTwo {
+    public static void main(String[] args) {
+        System.out.println("Введите игру: COIN или DICE ");
+        Scanner in = new Scanner(System.in);
+        String typeOfGame = in.nextLine().toUpperCase();
+        FactoryOfGames factory = new FactoryOfGames();
+        Game game = factory.makeGame(TypeOfGame.valueOf(typeOfGame));
+        game.tossUp();
+
+    }
+}
--- src/main/java/ru/kulichenkom/ekkel/chapter9/game/interfaces/GameFactory.java
+++
@@ -1,5 +0,0 @@
-package ru.kulichenkom.ekkel.chapter9.game.interfaces;
-
-public interface GameFactory {
-    Game makeGame();
-}
