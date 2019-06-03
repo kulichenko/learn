8bf691d2635cd422d092fb7bac770667306a6f9b
Maxim <kulichenkom@ya.ru>
2019-06-03 06:38:07 +0300

    Chapter 10 (внутренние классы) Ex1
    Signed-off-by: Maxim <kulichenkom@ya.ru>

---
+++ src/main/java/ru/kulichenkom/ekkel/chapter10/Outer.java
@@ -0,0 +1,26 @@
+//Ch10Ex1 Напишите класс с именем Outer, содержащий внутренний класс с именем Inner.
+//Добавьте в Outer метод, возвращающий объект типа Inner. В методе main() создайте и
+//инициализируйте ссылку на Inner.
+package ru.kulichenkom.ekkel.chapter10;
+
+public class Outer {
+    public Outer() {
+        System.out.println("Конструктор класса Outer");
+    }
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.getInner();
+    }
+
+    public Inner getInner() {
+        System.out.println("Возращение объекта Inner.");
+        return new Inner();
+    }
+
+    class Inner {
+        public Inner() {
+            System.out.println("Конструктор класса Inner");
+        }
+    }
+}
