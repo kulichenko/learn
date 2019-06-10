92bafb5dbabfb8df861c25e8825ca6580d3318a2
Maxim <kulichenkom@ya.ru>
2019-06-10 22:52:05 +0300

    Глава 10 (внутр-е классы) упражнение 9.
    Signed-off-by: Maxim <kulichenkom@ya.ru>

---
+++ src/main/java/ru/kulichenkom/ekkel/chapter10/Staff.java
@@ -0,0 +1,27 @@
+//Ch10Ex9 создайте интерфейс, содержащий минимум один метод.
+//Реализуйте его, определяя внутренний класс в методе, который возвращает
+//ссылку на ваш интерфейс.
+package ru.kulichenkom.ekkel.chapter10;
+
+interface ToDoWork {
+    void task();
+}
+
+public class Staff {
+    private String unit1 = "Tom Soyer";
+
+    public static void main(String[] args) {
+        Staff tomSoyer = new Staff();
+        tomSoyer.doWork().task();
+    }
+
+    public ToDoWork doWork() {
+        class PaintFence implements ToDoWork {
+            @Override
+            public void task() {
+                System.out.println(unit1 + " is painting the fence.");
+            }
+        }
+        return new PaintFence();
+    }
+}
