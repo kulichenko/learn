32dcef23efe3e9876b72f653c5638e5ce5185872
Maxim <kulichenkom@ya.ru>
2019-06-10 23:17:46 +0300

    Глава 10 (внутр-е классы) упражнение 10.
    Signed-off-by: Maxim <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/ekkel/chapter10/Staff.java
+++ src/main/java/ru/kulichenkom/ekkel/chapter10/Staff.java
@@ -1,6 +1,5 @@
-//Ch10Ex9 создайте интерфейс, содержащий минимум один метод.
-//Реализуйте его, определяя внутренний класс в методе, который возвращает
-//ссылку на ваш интерфейс.
+//Ch10Ex10 Повторите предыдущее упражнение (9-е, закомментировано ниже),
+//но определите внутренний класс в области действия внутри метода.
 package ru.kulichenkom.ekkel.chapter10;
 
 interface ToDoWork {
@@ -12,16 +11,47 @@ public class Staff {
 
     public static void main(String[] args) {
         Staff tomSoyer = new Staff();
-        tomSoyer.doWork().task();
+        tomSoyer.doWork(true).task();
     }
 
-    public ToDoWork doWork() {
-        class PaintFence implements ToDoWork {
-            @Override
-            public void task() {
-                System.out.println(unit1 + " is painting the fence.");
+    public ToDoWork doWork(boolean availabilityOfPaint) {
+        if (availabilityOfPaint) {
+            class PaintFence implements ToDoWork {
+                @Override
+                public void task() {
+                    System.out.println(unit1 + " is painting the fence.");
+                }
             }
+            return new PaintFence();
         }
-        return new PaintFence();
+        return null;
     }
 }
+
+//Ch10Ex9 создайте интерфейс, содержащий минимум один метод.
+//Реализуйте его, определяя внутренний класс в методе, который возвращает
+//ссылку на ваш интерфейс.
+//package ru.kulichenkom.ekkel.chapter10;
+//
+//interface ToDoWork {
+//    void task();
+//}
+//
+//public class Staff {
+//    private String unit1 = "Tom Soyer";
+//
+//    public static void main(String[] args) {
+//        Staff tomSoyer = new Staff();
+//        tomSoyer.doWork().task();
+//    }
+//
+//    public ToDoWork doWork() {
+//        class PaintFence implements ToDoWork {
+//            @Override
+//            public void task() {
+//                System.out.println(unit1 + " is painting the fence.");
+//            }
+//        }
+//        return new PaintFence();
+//    }
+//}
