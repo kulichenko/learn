f35efcf4531abe5a5e23485ec42b87d51df77f59
Maxim <kulichenkom@ya.ru>
2019-07-14 21:44:27 +0300

    CH11 EX 16
    Signed-off-by: Maxim <kulichenkom@ya.ru>

---
+++ src/main/java/ru/kulichenkom/ekkel/chapter11/Exercise16.java
@@ -0,0 +1,42 @@
+//Ch11Ex16 Создайте контейнер Set со всеми гласными буквами. Подсичтайте и выведите количество
+//гласных букв в каждом входном слове, а так же выведите общее количесво гласных во входном файле.
+
+package ru.kulichenkom.ekkel.chapter11;
+
+import ru.kulichenkom.utils.TextFile;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Exercise16 {
+    public static void vowelCounter(Set<String> setString) {
+        Set<Character> vowelLetters = new HashSet<>();
+        Collections.addAll(vowelLetters, 'A', 'E', 'I', 'O', 'U', 'Y', 'a', 'e', 'i', 'o', 'u', 'y');
+        Set<String> numbers = new HashSet<>();
+        Collections.addAll(numbers, "0 1 2 3 4 5 6 7 8 9".split(" "));
+        setString.removeAll(numbers);
+        int totalVowels = 0;
+        for (String s : setString) {
+            int count = 0;
+            for (Character v : s.toCharArray()) {
+                if (vowelLetters.contains(v)) {
+                    count++;
+                    totalVowels++;
+                }
+            }
+            System.out.print(s + ": " + count + ", ");
+        }
+        System.out.println();
+        System.out.print("Total vowels: " + totalVowels);
+    }
+
+    public static void main(String[] args) {
+
+        Set<String> words = new TreeSet<>(new TextFile("src/main/java" +
+                "/ru/kulichenkom/ekkel/chapter11/Gerbil.java", "\\W+"));
+        System.out.println(words);
+        vowelCounter(words);
+    }
+}
