cb31e562f838d0152ec98114626bd8cf1172a44d
Maxim <kulichenkom@ya.ru>
2019-08-14 22:21:40 +0300

    Chapter 12 Ex4
    Signed-off-by: Maxim <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/ekkel/chapter12/MyException.java
+++ src/main/java/ru/kulichenkom/ekkel/chapter12/MyException.java
@@ -1,3 +1,6 @@
+//Ch12Ex4 Создайте свой собственный класс исключения. Напишите конструктор, получающий строковый аргумент,
+//и сохраните этот аргумент внутри объекта по ссылке на String. Напишите метод, который выводит эту строку.
+//Подсоедините новый блок try-catch, чтобы проверить нов искл-е.
 package ru.kulichenkom.ekkel.chapter12;
 
 public class MyException extends Exception {
@@ -5,11 +8,26 @@ public class MyException extends Exception {
 
     public MyException(String description) {
         super(description);
-        System.out.println(" MyException()");
         this.description = description;
     }
 
     void show() {
-        System.out.println("Description from MyException: " + description);
+        System.out.println("Исключение MyException: " + description);
+    }
+}
+
+class Checker {
+    static void getNewException() throws MyException {
+        System.out.println("getNewException()");
+        throw new MyException(" Возбуждение исключения из getNewException() ");
+    }
+
+    public static void main(String[] args) {
+        try {
+            getNewException();
+        } catch (MyException e) {
+            e.printStackTrace();
+            e.show();
+        }
     }
 }
