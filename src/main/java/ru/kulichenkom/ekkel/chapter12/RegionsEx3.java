c035ded256e4f25211636fce76c80912797cb37a
kulichenko <kulichenkom@ya.ru>
2019-08-19 15:47:46 +0300

    CH12Ex7
    Signed-off-by: kulichenko <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/ekkel/chapter12/RegionsEx3.java
+++ src/main/java/ru/kulichenkom/ekkel/chapter12/RegionsEx3.java
@@ -1,13 +1,24 @@
+//Ch12Ex7Измените упражнение 3 так, чтобы информация об исключении выводилась в блоке catch
 //Ch12Ex3 Напишите код, который генерирует и перехватывает исключение
 //ArrayIndexOutOfBoundsException.
 package ru.kulichenkom.ekkel.chapter12;
 
+import javax.swing.*;
+
 public class RegionsEx3 {
-    int[] numberOfRegions = new int[89];
+    static int[] numberOfRegions = new int[89];
 
     public static void main(String[] args) {
         RegionsEx3 regions = new RegionsEx3();
-        System.out.println(regions.getNumberOfRegions(89));
+        for (int i = 0; i < numberOfRegions.length; i++) {
+            numberOfRegions[i] = i + 1;
+        }
+        try {
+            System.out.println(regions.getNumberOfRegions(numberOfRegions[numberOfRegions.length]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("Массив не содержит числа с индексом " + numberOfRegions.length);
+        }
     }
 
     public int getNumberOfRegions(int i) {
