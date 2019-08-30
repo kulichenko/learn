f2a7cf39ac5e5c6db1a7a8a5967ecddd60264709
kulichenko <kulichenkom@ya.ru>
2019-08-30 15:37:30 +0300

    CH13Ex9
    Signed-off-by: kulichenko <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/ekkel/chapter13/Splitting.java
+++ src/main/java/ru/kulichenkom/ekkel/chapter13/Splitting.java
@@ -1,9 +1,11 @@
 //Ch13Ex8 Разбейте строку Splitting.knights по словам the или you.
+//Ch13Ex9 Замените все глассные в Splitting.knights подчеркиваниями.
 package ru.kulichenkom.ekkel.chapter13;
 
 import java.util.*;
 
 public class Splitting {
+    private static final String VOWELS = "[AaEeIiOoUuYy]";
     public static String knights =
             "Then when you have found the shrubbery you must " +
                     "cut down the mightiest tree in the forest... " +
@@ -14,10 +16,16 @@ public class Splitting {
                 Arrays.toString(knights.split(regex)));
     }
 
+    public static void replace(String regex, String replacer) {
+        System.out.println(Arrays.toString(new String[]{knights.replaceAll(regex, replacer)}));
+    }
+
     public static void main(String[] args) {
+        System.out.println(knights);
 //        split(" "); // Doesn't have to contain regex chars
 //        split("\\W+"); // Non-word characters
 //        split("n\\W+"); // 'n' followed by non-word characters
         split("the\\W+|you\\W+");
+        replace(VOWELS, "_");
     }
 }
