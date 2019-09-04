3e28d37fee38f38a0e0daa521e139e5198a6a05a
Maxim <kulichenkom@ya.ru>
2019-09-04 23:24:43 +0300

    Ch13Ex15 чуть изменил
    Signed-off-by: Maxim <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/ekkel/chapter13/JGrep15.java
+++ src/main/java/ru/kulichenkom/ekkel/chapter13/JGrep15.java
@@ -11,30 +11,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JGrep15 {
+    public static int getFlagFromArgumentForPattern(String arg) {
+        int flag = 0;
+        switch (arg) {
+            case "Pattern.CASE_INSENSITIVE":
+                flag = Pattern.CASE_INSENSITIVE;
+                break;
+            case "Pattern.CANON_EQ":
+                flag = Pattern.CANON_EQ;
+                break;
+            case "Pattern.COMMENTS":
+                flag = Pattern.COMMENTS;
+                break;
+            case "Pattern.DOTALL":
+                flag = Pattern.DOTALL;
+                break;
+            case "Pattern.LITERAL":
+                flag = Pattern.LITERAL;
+                break;
+            case "Pattern.MULTILINE":
+                flag = Pattern.MULTILINE;
+                break;
+            case "Pattern.UNICODE_CASE":
+                flag = Pattern.UNICODE_CASE;
+                break;
+            case "Pattern.UNIX_LINES":
+                flag = Pattern.UNIX_LINES;
+                break;
+        }
+        return flag;
+    }
 
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
             System.out.println("Usage: java JGrep file regex");
             System.exit(0);
         }
-        int flag = 0;
-        if (args[2].equals("Pattern.CASE_INSENSITIVE"))
-            flag = Pattern.CASE_INSENSITIVE;
-        else if (args[2].equals("Pattern.CANON_EQ"))
-            flag = Pattern.CANON_EQ;
-        else if (args[2].equals("Pattern.COMMENTS"))
-            flag = Pattern.COMMENTS;
-        else if (args[2].equals("Pattern.DOTALL"))
-            flag = Pattern.DOTALL;
-        else if (args[2].equals("Pattern.LITERAL"))
-            flag = Pattern.LITERAL;
-        else if (args[2].equals("Pattern.MULTILINE"))
-            flag = Pattern.MULTILINE;
-        else if (args[2].equals("Pattern.UNICODE_CASE"))
-            flag = Pattern.UNICODE_CASE;
-        else if (args[2].equals("Pattern.UNIX_LINES"))
-            flag = Pattern.UNIX_LINES;
-        Pattern p = Pattern.compile(args[1], flag);
+
+        String flag = args[2];
+        Pattern p = Pattern.compile(args[1], getFlagFromArgumentForPattern(flag));
         int index = 0;
         Matcher m = p.matcher("");
         Path path = Paths.get(args[0]);
