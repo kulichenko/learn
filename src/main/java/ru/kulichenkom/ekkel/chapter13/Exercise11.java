f858b83d129027432f943f4844ec888b39267445
Maxim <kulichenkom@ya.ru>
2019-09-04 21:48:51 +0300

    Chapter 13 Ex11
    Signed-off-by: Maxim <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/ekkel/chapter13/Exercise11.java
+++ src/main/java/ru/kulichenkom/ekkel/chapter13/Exercise11.java
@@ -5,7 +5,6 @@ package ru.kulichenkom.ekkel.chapter13;
 
 public class Exercise11 {
     public static void main(String[] args) {
-        Exercise10 exercise10 = new Exercise10();
         String text = "Arline ate eight apples and one orange while Anita hadn’t any";
         String[] regularEx = {"(?i)((^[aeiou])|(\\s+[aeiou]))\\w+?[aeiou]\\b"};
         Exercise10.checkerForMatching(text, regularEx);
