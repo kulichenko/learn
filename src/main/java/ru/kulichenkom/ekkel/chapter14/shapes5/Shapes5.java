7dc188367408e24fbb9eaa3ab6344cf3087fce4c
Maxim <kulichenkom@ya.ru>
2019-09-11 21:41:28 +0300

    Ch14Ex5
    Signed-off-by: Maxim <kulichenkom@ya.ru>

---
+++ src/main/java/ru/kulichenkom/ekkel/chapter14/shapes5/Shapes5.java
@@ -0,0 +1,52 @@
+//Ch14Ex5
+package ru.kulichenkom.ekkel.chapter14.shapes5;
+
+import java.util.Arrays;
+import java.util.List;
+
+abstract class Shape {
+    void draw() {
+        System.out.println(this + ".draw()");
+    }
+
+    abstract public String toString();
+}
+
+class Circle extends Shape {
+    public String toString() {
+        return "Circle";
+    }
+}
+
+class Square extends Shape {
+    public String toString() {
+        return "Square";
+    }
+}
+
+class Triangle extends Shape {
+    public String toString() {
+        return "Triangle";
+    }
+}
+
+public class Shapes5 {
+
+    public static void rotate(Shape shape) {
+        if (!(shape instanceof Circle)) {
+            System.out.println(shape + ".rotate()");
+        } else {
+            System.out.println(shape + " can not rotate()");
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Shape> shapeList = Arrays.asList(
+                new Circle(), new Square(), new Triangle()
+        );
+        for (Shape shape : shapeList) {
+            shape.draw();
+            rotate(shape);
+        }
+    }
+}
