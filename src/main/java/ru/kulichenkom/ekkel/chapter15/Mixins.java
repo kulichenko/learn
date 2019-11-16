2950ea49dcbbe416d768783c4dc1c3d3ecfd2c02
Maxim <kulichenkom@ya.ru>
2019-11-16 21:57:02 +0300

    Ch15Ex37
    Signed-off-by: Maxim <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/ekkel/chapter15/Mixins.java
+++ src/main/java/ru/kulichenkom/ekkel/chapter15/Mixins.java
@@ -1,8 +1,30 @@
+//Ch15Ex37 Добавьте в Mixins.java новый класс примеси Colored. Подмешайте его в Mixin
+// и покажите, что программа работает.
 package ru.kulichenkom.ekkel.chapter15;
 //github.com/BruceEckel/TIJ4-code/blob/master/examples/generics/Mixins.java
 //: generics/Mixins.java
 
 import java.util.Date;
+
+interface Color {
+    String getColor();
+
+    void setColor(String color);
+}
+
+class Colored implements Color {
+    private String color;
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
 
 interface TimeStamped {
     long getStamp();
@@ -54,9 +76,10 @@ class BasicImp implements Basic {
 //class Mixin фактически использует деленирование, так что каждый примесный тип требует поля в Mixin.
 //и выдолжны написать все необходимые методы в Mixin для передачи вызовов соответствующим объектам .
 //При более сложных примесях код быстро разрастается.
-class Mixin extends BasicImp implements TimeStamped, SerialNumbered {
+class Mixin extends BasicImp implements TimeStamped, SerialNumbered, Color {
     private TimeStamped timeStamp = new TimeStampedImp();
     private SerialNumbered serialNumber = new SerialNumberedImp();
+    private Color color = new Colored();
 
     public long getStamp() {
         return timeStamp.getStamp();
@@ -65,6 +88,16 @@ class Mixin extends BasicImp implements TimeStamped, SerialNumbered {
     public long getSerialNumber() {
         return serialNumber.getSerialNumber();
     }
+
+    @Override
+    public String getColor() {
+        return color.getColor();
+    }
+
+    @Override
+    public void setColor(String val) {
+        color.setColor(val);
+    }
 }
 
 public class Mixins {
@@ -72,13 +105,12 @@ public class Mixins {
         Mixin mixin1 = new Mixin(),
                 mixin2 = new Mixin();
         mixin1.set("test string 1");
+        mixin1.setColor("Green");
         mixin2.set("test string 2");
+        mixin2.setColor("Yellow");
         System.out.println(mixin1.get() + " " +
-                mixin1.getStamp() + " " + mixin1.getSerialNumber());
+                mixin1.getStamp() + " " + mixin1.getSerialNumber() + " " + mixin1.getColor());
         System.out.println(mixin2.get() + " " +
-                mixin2.getStamp() + " " + mixin2.getSerialNumber());
+                mixin2.getStamp() + " " + mixin2.getSerialNumber() + " " + mixin2.getColor());
     }
-} /* Output: (Sample)
-test string 1 1132437151359 1
-test string 2 1132437151359 2
-*///:~
+}
