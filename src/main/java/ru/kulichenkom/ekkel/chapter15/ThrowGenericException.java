dcc3ee9f43d73358f3574e1b038cc9bbdf19e316
Maxim <kulichenkom@ya.ru>
2019-11-16 21:09:46 +0300

    Ch15Ex36
    Signed-off-by: Maxim <kulichenkom@ya.ru>

--- src/main/java/ru/kulichenkom/ekkel/chapter15/ThrowGenericException.java
+++ src/main/java/ru/kulichenkom/ekkel/chapter15/ThrowGenericException.java
@@ -1,3 +1,5 @@
+//Ch15Ex36 Добавьте в класс Processor второе параметризированное исключение
+//и продемонстрируйте возможность независимого изменения исключений
 package ru.kulichenkom.ekkel.chapter15;
 //: generics/ThrowGenericException.java
 //github.com/BruceEckel/TIJ4-code/blob/master/examples/generics/ThrowGenericException.java
@@ -7,16 +9,16 @@ import java.util.List;
 
 //интерфейс Processor выполняет process() и может возбудить исключение типа E.
 //Результат process сохраняется в List<T> resultCollector.
-interface Processor<T, E extends Exception> {
-    void process(List<T> resultCollector) throws E;
+interface Processor<T, E extends Exception, F extends Exception> {
+    void process(List<T> resultCollector) throws E, F;
 }
 
 //ProcessRunner содержит метод processAll(), который выполняет каждый содержащийся в нем объект
 //Process и возвращает resultCollector.
-class ProcessRunner<T, E extends Exception> extends ArrayList<Processor<T, E>> {
-    List<T> processAll() throws E {
+class ProcessRunner<T, E extends Exception, F extends Exception> extends ArrayList<Processor<T, E, F>> {
+    List<T> processAll() throws E, F {
         List<T> resultCollector = new ArrayList<>();
-        for (Processor<T, E> processor : this) {
+        for (Processor<T, E, F> processor : this) {
             processor.process(resultCollector);
         }
         return resultCollector;
@@ -26,10 +28,13 @@ class ProcessRunner<T, E extends Exception> extends ArrayList<Processor<T, E>> {
 class Failure1 extends Exception {
 }
 
-class Processor1 implements Processor<String, Failure1> {
+class Failure2 extends Exception {
+}
+
+class Processor1 implements Processor<String, Failure1, Failure2> {
     static int count = 3;
 
-    public void process(List<String> resultCollector) throws Failure1 {
+    public void process(List<String> resultCollector) throws Failure1, Failure2 {
         if (count-- > 1) {
             resultCollector.add("Hep!");
         } else {
@@ -38,16 +43,17 @@ class Processor1 implements Processor<String, Failure1> {
         if (count < 0) {
             throw new Failure1();
         }
+        if (count == 0) {
+            throw new Failure2();
+        }
     }
 }
 
-class Failure2 extends Exception {
-}
 
-class Processor2 implements Processor<Integer, Failure2> {
+class Processor2 implements Processor<Integer, Failure1, Failure2> {
     static int count = 2;
 
-    public void process(List<Integer> resultCollector) throws Failure2 {
+    public void process(List<Integer> resultCollector) throws Failure1, Failure2 {
         if (count-- == 0) {
             resultCollector.add(47);
         } else {
@@ -56,12 +62,15 @@ class Processor2 implements Processor<Integer, Failure2> {
         if (count < 0) {
             throw new Failure2();
         }
+        if (count == 0) {
+            throw new Failure1();
+        }
     }
 }
 
 public class ThrowGenericException {
     public static void main(String[] args) {
-        ProcessRunner<String, Failure1> runner = new ProcessRunner<>();
+        ProcessRunner<String, Failure1, Failure2> runner = new ProcessRunner<>();
         for (int i = 0; i < 3; i++) {
             runner.add(new Processor1());
         }
@@ -69,9 +78,11 @@ public class ThrowGenericException {
             System.out.println(runner.processAll());
         } catch (Failure1 e) {
             System.out.println(e);
+        } catch (Failure2 failure2) {
+            System.out.println(failure2);
         }
 
-        ProcessRunner<Integer, Failure2> runner2 = new ProcessRunner<>();
+        ProcessRunner<Integer, Failure1, Failure2> runner2 = new ProcessRunner<>();
         for (int i = 0; i < 3; i++) {
             runner2.add(new Processor2());
         }
@@ -79,6 +90,8 @@ public class ThrowGenericException {
             System.out.println(runner2.processAll());
         } catch (Failure2 e) {
             System.out.println(e);
+        } catch (Failure1 failure1) {
+            System.out.println(failure1);
         }
     }
 } ///:~
